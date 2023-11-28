1afd4ca85b614a6932e4cde9416b077f251981fb
Josh Rhoades <josh@axiomdatascience.com>
2023-11-28 15:47:52 -0300

    First working implementation using YOLO best_seal.pt model

--- yolo_processing.py
+++ yolo_processing.py
@@ -1,4 +1,5 @@
 import os
+from typing import List, Set
 import cv2
 import numpy as np
 from ultralytics import YOLO
@@ -11,8 +12,8 @@ logger = logging.getLogger( __name__ )
 
 width = 896
 height = 896
-# threshold = 0.5
-# font = cv2.FONT_HERSHEY_SIMPLEX
+threshold = 0.5
+font = cv2.FONT_HERSHEY_SIMPLEX
 
 MODEL_FOLDER = Path(os.environ.get(
     "MODEL_DIRECTORY",
@@ -21,13 +22,13 @@ MODEL_FOLDER = Path(os.environ.get(
 
 YOLO_MODELS = {
     "best_seal": {
-        "1": YOLO(str(MODEL_FOLDER / "best_seal.pt" )),
+        "1": YOLO(str(MODEL_FOLDER / "yolo" / "best_seal" / "1" / "best_seal.pt" )),
     }
 }
 
 
 def yolo_process_image(
-    yolo_model,
+    yolo_model: YOLO,
     output_path: Path,
     model: str,
     version: str,
@@ -47,63 +48,59 @@ def yolo_process_image(
             "and be a directory"
         )
 
-    # npdata = np.asarray(bytearray(bytedata), dtype="uint8")
-    # image = cv2.imdecode(npdata, cv2.IMREAD_COLOR)
-    # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    # resized = cv2.resize(image, (width , height))
-    # rgb_tensor = tf.convert_to_tensor(resized, dtype=tf.uint8)
-    # rgb_tensor = tf.expand_dims(rgb_tensor, 0)
+    output_file = output_path / model / str(version) / name
 
-    # # Run the model, get the results
-    # boxes, scores, _classes, _num_detections = yolo_model(rgb_tensor)
+    npdata = np.asarray(bytearray(bytedata), dtype="uint8")
+    frame = cv2.imdecode(npdata, cv2.IMREAD_COLOR)
 
-    # output_file = output_path / model / str(version) / name
+    img_boxes = frame
 
-    # h, w, _ = image.shape
+    #use YOLOv8
+    results = yolo_model.predict(frame, conf = 0.2)
 
-    # boxes = boxes.numpy()[0].astype('int')
-    # scores = scores.numpy()[0]
+    # If any score is above threshold, flag it as detected
+    detected = False
+    scores: List[float] = []
+    classes: Set[float] = set()
 
-    # # If any score is above threshold, flag it as detected
-    # detected = False
+    for result in results:
+        for score, cls, bbox in zip(result.boxes.conf, result.boxes.cls, result.boxes.xyxy):
 
-    # # Draw the results if they are above a defined threshold
-    # for score, (ymin, xmin, ymax, xmax) in zip(scores, boxes):
+            scores.append( score.item() )
+            classes.add( cls.item() )
 
-    #     if score < threshold:
-    #         continue
+            if score < threshold:
+                continue
 
-    #     detected = True
+            detected = True
 
-    #     y_min = int(max(1, (ymin * (h / height))))
-    #     x_min = int(max(1, (xmin * (w / width))))
-    #     y_max = int(min(h, (ymax * (h / height))))
-    #     x_max = int(min(w, (xmax * (w / width))))
+            x1, y1, x2, y2 = bbox[0].item(), bbox[1].item(), bbox[2].item(), bbox[3].item()
+            h, w, _ = frame.shape
 
-    #     cv2.rectangle(
-    #         image,
-    #         (x_min, y_max),
-    #         (x_max, y_min),
-    #         (255, 0, 255, 255),
-    #         thickness=2
-    #     )
+            y_min = int(max(1, y1))
+            x_min = int(max(1, x1))
+            y_max = int(min(h, y2))
+            x_max = int(min(w, x2))
 
-    #     score = round(100 * score, 0)
-    #     label = f"Seal::{score}%"
-    #     cv2.putText(
-    #         image,
-    #         label,
-    #         (x_min, y_max + 50),
-    #         font,
-    #         1,
-    #         (255, 0, 255, 255),
-    #         2,
-    #         cv2.LINE_AA
-    #     )
+            #label = "Seal" + ": " + ": {:.2f}%".format(score * 100)
 
-    # if detected is True:
-    #     output_file.parent.mkdir(parents=True, exist_ok=True)
-    #     cv2.imwrite(str(output_file), image)
-    #     return str(output_file)
+            if cls.item() == 0.0:
+                label = "Rock" + ": " + ": {:.2f}%".format(score * 100)
+                img_boxes = cv2.rectangle(img_boxes, (x_min, y_max), (x_max, y_min), (0, 255, 0), 2)
+                cv2.putText(img_boxes, label, (x_min, y_max - 10), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
+            if cls.item() == 1.0:
+                label = "Seal" + ": " + ": {:.2f}%".format(score * 100)
+                img_boxes = cv2.rectangle(img_boxes, (x_min, y_max), (x_max, y_min), (0, 0, 255), 2)
+                cv2.putText(img_boxes, label, (x_min, y_max - 10), font, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+
+    outp = cv2.resize(img_boxes, (1280, 720))
+
+    logger.warning( f"Scores: {repr( scores )}" )
+    logger.warning( f"Classes: {repr( classes )}" )
+
+    if detected is True:
+        output_file.parent.mkdir(parents=True, exist_ok=True)
+        cv2.imwrite(str(output_file), outp )
+        return str(output_file)
 
     return None
